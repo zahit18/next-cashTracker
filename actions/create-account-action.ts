5bd13da84aa746eaae611c5611e4dc2c39b9b05a
zahit18 <zahithdez@gmail.com>
2025-05-07 20:47:33 -0700

    Create Account

--- actions/create-account-action.ts
+++ actions/create-account-action.ts
@@ -8,7 +8,7 @@ export async function register(formData: FormData) {
         name: formData.get('name'),
         email: formData.get('email'),
         password: formData.get('password'),
-        password_confirmation: formData.get('passsword_confirmation'),
+        password_confirmation: formData.get('password_confirmation'),
     }
 
     // validat
@@ -17,6 +17,26 @@ export async function register(formData: FormData) {
     console.log(register)
     console.log(errors)
 
+    if(!register.success) return
+    // if (!register.success) {
+    //     throw new Error(register.error.errors.map(e => e.message).join(', '))
+    // }
+
     // registrar usuario
     const url = `${process.env.API_URL}/auth/create-account`
+    const req = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: register.data?.name,
+            password: register.data?.password,
+            email: register.data?.email
+        })
+    })
+
+    const json = await req.json()
+
+    console.log(json)
 }
